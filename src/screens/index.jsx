ddcb6288592e374674a63a75582eb426c0343e1d
M.Bilal Khalid <94046237+mbilalkhalidofficial@users.noreply.github.com>
2022-11-21 14:51:29 +0500

    most luxury done

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,20 +1,54 @@
-import { Head, Link } from "router";
-
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import most__luxuay__left from "../assets/most__luxuay__left.png";
+import most__luxuay__right from "../assets/most__luxuay__right.png";
 
-export const loader = async () => {
-  return "data from loader";
-};
-
-export default function Index() {
-  const data = useLoaderData();
+export default function index() {
   return (
-    <div>
-      <Head title="Home | Demo Template" description="Home | Demo Template" />
-      index
-      <div>{data}</div>
-      <Link to="/about">About</Link>
-    </div>
+    <>
+      <div className="most__luxury">
+        <div className="most__luxury__left">
+          <div className="most__luxury__left__img">
+            <img
+              className="most__luxury__left__image"
+              src={most__luxuay__left}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="most__luxury__right">
+          <div className="most__luxury__right__main__heading">
+            <span>Most luxury </span> resorts for more discering
+          </div>
+          <div className="most__luxury__right__para">
+            Dream Home is a gated community with a great location. Located
+            downtown, you’re within walking distance of Parks, and the best
+            shopping, dining and entertainment Getting around is a breeze, with
+            easy access to freeways, buses and trolleys. . Laundry is available
+            on premises.Dream Home is a gated community with a great location.
+            Located downtown, you’re within walking distance of Parks, and the
+            best shopping, dining and entertainment Getting around is a breeze,
+            with easy access to freeways, buses and trolleys. . Laundry is
+            available on premises.
+          </div>
+          <div className="most__luxury__right__button">Read More</div>
+          <div className="most__luxury__right__pics">
+            <div className="most__luxury__right__picture">
+              <img
+                className="most__luxury__left__image"
+                src={most__luxuay__right}
+                alt=""
+              />
+            </div>
+            <div className="most__luxury__right__picture">
+              <img
+                className="most__luxury__left__image"
+                src={most__luxuay__right}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
