3c42c358b065e81c9548295f8348400fb3bec6c4
M.Bilal Khalid <94046237+mbilalkhalidofficial@users.noreply.github.com>
2022-11-25 12:44:25 +0500

    location__features

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -90,7 +90,7 @@ export default function index() {
           </div>
         </div>
       </div>
-      ////
+
       {/* <div className="our__best__luxury">
         <div className="our__best__luxury__left">
           <div className="our__best__luxury__left__text">
@@ -279,6 +279,41 @@ export default function index() {
           </div>
         </div>
       </section>
+      <div className="location__features">
+        <div className="location__features__left">
+          <div className="location__features__left__heading">
+            <span>Location</span> features
+          </div>
+          <div className="location__features__left__para">
+            Dream Home is a gated community with a great location. Located
+            downtown, you’re within walking distance of Parks, and the best
+            shopping, dining and entertainment Getting around is a breeze, with
+            easy access to freeways, buses and trolleys. . Laundry is available
+            on premises.
+          </div>
+          <div className="location__features__left___content">
+            <div className="location__features__left__content_text">
+              Located just in front of tourism highway.
+            </div>
+            <div className="location__features__left__content_text">
+              Just 1.5 km away from Patriata Chair Lift.
+            </div>
+            <div className="location__features__left__content_text">
+              Sea level Elevation from 4000 to 6000 feet.
+            </div>
+          </div>
+          <div className="most__luxury__right__button">Read More</div>
+        </div>
+        <div className="most__luxury__left">
+          <div className="most__luxury__left__content">
+            <img
+              className="most__luxury__img"
+              src={locationfeaturesimg}
+              alt="locationfeaturesimg"
+            />
+          </div>
+        </div>
+      </div>
       {/* <section className="locationpointer">
         <div className="locationpointer__left">
           <div className="locationpointer__left__Content">
